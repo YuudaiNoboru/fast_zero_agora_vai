58fc8ca6c9a200fb1bc8222884e611a4ef40a975
Lucas Jose Santos Vasconcelos <lucas.vasconcelos@codevasf.gov.br>
2025-03-17 17:56:20 -0300

    Atualização da tabela User.

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,7 +7,9 @@ from fast_zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(modal=User) as time:
-        new_user = User(username='alice', password='secret', email='teste@test')
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
         session.add(new_user)
         session.commit()
 
@@ -18,4 +20,5 @@ def test_create_user(session, mock_db_time):
             'username': 'alice',
             'email': 'teste@teste',
             'created_at': time,
+            'update_at': time,
         }
